d0ecfb2d4afa2c6aabe44509964ada8d9489a10d
Jack Price <jack.s.price03@outlook.com>
2022-11-16 11:09:17 +0000

    Challenge one, added loop

--- ca_fire_growth_experiment.py
+++ ca_fire_growth_experiment.py
@@ -14,15 +14,19 @@ PROB_GROWTH = 1.0e-4  # probability of new growth per cell per unit time
 PROB_NEW_FIRE = 1.0e-6  # probability of new fire per cell per unit time
 N_TIME_STEP = 20000  # number of time-steps
 VERBOSE = True # set to False to suppress printing
+PROB_GROWTH_LIST = [1e+00, 1e-01, 1e-02, 1e-03, 1e-04, 1e-05]
 
 # run the model
-result = evolve_forest(
-    (GRID_N_X, GRID_N_X), N_TIME_STEP, PROB_GROWTH, PROB_NEW_FIRE, verbose=VERBOSE)
+for prob_growth in PROB_GROWTH_LIST:
+    result = evolve_forest(
+        (GRID_N_X, GRID_N_X), N_TIME_STEP, prob_growth, PROB_NEW_FIRE, 
+        verbose=VERBOSE)
+
 
 #output
-file_name = f'forest_grid_{N_TIME_STEP:06d}.nc'
-
-if VERBOSE:
-    print(f'writing final output to {file_name}')
-
-write_netcdf(file_name , *result)
+    file_name = f'forest_pg_{prob_growth:1.0e}_{N_TIME_STEP:06d}.nc'
+    
+    if VERBOSE:
+        print(f'writing final output to {file_name}')
+    
+    write_netcdf(file_name , *result)
